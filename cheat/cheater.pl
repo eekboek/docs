d8f68ced742a239f0e9b9212185db7ca917827aa
Johan Vromans <jvromans@squirrel.nl>
2012-01-14 17:15:21 +0100

    Fixes.

--- cheat/cheater.pl
+++ cheat/cheater.pl
@@ -7,6 +7,9 @@ use Encode;
 
 open(my $ix,  ">", "ix.html"     );
 open(my $det, ">", "details.html");
+open(my $tt2, ">", "ix.tt2"      );
+my $top;
+
 #binmode( STDIN, ":encoding(utf-8)");
 
 print {$ix} ("<head>\n",
@@ -17,6 +20,13 @@ print {$det} ("<head>\n",
 	      "<link rel=\"stylesheet\" href=\"cheat.css\">\n",
 	      "<head>\n",
 	      "<body>\n");
+print {$tt2} ("<li>\n",
+	      "<object type=\"text/sitemap\">\n",
+	      "<param name=\"Name\" value=\"Commando&rsquo;s\">\n",
+	      "<param name=\"Local\" value=\"details.html\">\n",
+	      "</object>\n",
+	      "<ul>\n");
+
 
 my $did = 0;
 while ( <> ) {
@@ -26,8 +36,15 @@ while ( <> ) {
 	my $target = $1;
 	print {$ix} ("<br>") if $did;
 	print {$ix} ("<a target=\"details\" href=\"details.html#$target\"><span class=\"tg\">$target</span></a>\n");
+	print {$tt2} ("[% PROCESS item title=\"$target\" link=\"topics/$target.html\" %]\n");
 	print {$det} ("</pre>\n<hr/>\n") if $did;
 	print {$det} ("<a name=\"$target\"><span class=\"h3\">$target</span></a>\n<pre>");
+	print {$top} ("</pre>\n") if $top;
+	undef $top;
+	open( $top, ">", "topics/$target.html"      );
+	print {$top} ("[% META type = \"text\" -%]\n",
+		      "<h2>$target</h2>\n",
+		      "<pre>");
 	$did++;
 	next;
     }
@@ -47,11 +64,14 @@ while ( <> ) {
     s;&lt\;(\w+)&gt\;;<var>$1</var>;g;
     s;&quot\;help (\w+)&quot\;;<a href="#$1">$1</a>;g;
     print {$det} encode("utf-8", $_), "\n";
+    print {$top} encode("utf-8", $_), "\n";
 }
 
 print {$det} ("</pre>\n<hr/>\n") if $did;
+print {$top} ("</pre>\n") if $top;
 print {$ix} ("</body>\n");
 print {$det} ("</body>\n");
+print {$tt2} ("</ul>\n");
 
 sub varfix {
     my $need = 0;
